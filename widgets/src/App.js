1410e0d178d9c40549c4e14ab1116dc4cd45234c
Hank H'ng <hankhng@hotmail.com>
2022-06-15 12:25:03 +0100

    Search widget wrap up

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is FE JS Library",
-  },
-  {
-    title: "Why use React?",
-    content: "It is a favourite JS library amongst engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating reusable components",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is FE JS Library",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "It is a favourite JS library amongst engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating reusable components",
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
---
+++ widgets/src/components/Search.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const Search = () => {
+  const [term, setTerm] = useState('Wikipedia');
+  const [debouncedTerm, setdebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setdebouncedTerm(term)
+    }, 1000);
+
+    // clean up function to clear the timeout
+    // it's not called the first time the component is rendered
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [term])
+
+  useEffect(() => {
+    const search = async () => {
+      // const { data } is object destructuring the returned results
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      })
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className='ui button'
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
+        </div>
+      </div>
+    )
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className='ui celled list'>
+        {renderedResults}
+      </div>
+    </div>
+  );
+}
+
+export default Search;
